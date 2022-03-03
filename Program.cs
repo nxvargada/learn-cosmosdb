d8760ea52115b6bf2940157a459d400863cd5d6c
Daniel Varga <varga.daniel@nexius.hu>
2022-03-03 08:10:36 +0100

    add cosmosdb config

--- Program.cs
+++ Program.cs
@@ -1,6 +1,15 @@
+using Microsoft.Azure.Cosmos;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/documents", () => {
+    using (var client = new CosmosClient(endpoint, masterKey))
+    {
+        
+    }
+});
 
 app.Run();
